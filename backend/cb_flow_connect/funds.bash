27196a1cebafc0cbe43f93395ca81490d3222cba
JBunCE <sr.conejo.uwu@gmail.com>
2025-07-14 20:12:49 -0600

    HotFix/Rename application name

--- backend/cb_flow_connect/funds.bash
+++ backend/cb_flow_connect/funds.bash
@@ -2,16 +2,16 @@
 # Requires:
 # - httpie: https://github.com/jkbrzt/httpie
 
-URL="https://stage.buildwithjuno.com/mint_platform/v1/clabes"
-API_KEY=""             # Your Juno API Key
-API_SECRET=""   # Your Juno API Secret
+URL="https://stage.buildwithjuno.com/spei/test/deposits"
+API_KEY="" # Your Juno API Key
+API_SECRET="" # Your Juno API Secret
 
 SECS=$(date +%s)
 DNONCE=$(expr 1000 '*' "$SECS")
 HTTPmethod=POST
-RequestPath="/mint_platform/v1/clabes"
+RequestPath="/spei/test/deposits"
 # JSON payload as a single string (no spaces, ensure it's compact)
-JSONPayload='{}'
+JSONPayload="{\"amount\":\"100\",\"receiver_clabe\":\"710969000000411444\",\"receiver_name\":\"Acme\",\"sender_name\":\"Acme\",\"sender_clabe\":\"710969000000411444\"}"
 
 # Create the signature
 SIGNATURE=$(echo -n $DNONCE$HTTPmethod$RequestPath$JSONPayload | openssl dgst -binary -sha256 -hmac $API_SECRET | xxd -p -c 256)
